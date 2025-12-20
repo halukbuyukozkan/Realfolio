f63d01b25414d95167680deea72bed3d16732e75
halukbuyukozkan <halukbuyukozkan@gmail.com>
2025-12-20 16:30:16 +0300

    Initialize Realfolio application with Spring Boot setup and update .gitignore

---
+++ src/main/java/com/buyukozkan/RealfolioApplication.java
@@ -0,0 +1,13 @@
+package com.buyukozkan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealfolioApplication.class, args);
+    }
+}
+
